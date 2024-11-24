1612e2f440441e605e4478de84dfc13773ceeca6
Md. Mohsin Kabir <mohsinkabirseo@gmail.com>
2024-11-24 18:14:45 +0600

    latest

--- src/components/containers/home/TestimonialSec.tsx
+++ src/components/containers/home/TestimonialSec.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
